52f4ce79a1915860a5f9a789b94f27b54a902b0c
Mersy-Dev <anuoluwafalaju@gmail.com>
2023-10-26 08:25:15 +0100

    Resetting UI, password next

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import avatar from '../Assets/profile.png';
-import styles from '../Styles/username.module.css'
+import styles from '../Styles/username.module.css';
+import {Toaster} from 'react-hot-toast';
+import {useFormik} from 'formik';
+import {usernameValidate} from '../Helper/Validate';
 
 
 
 export default function Username() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+    },
+    validate: usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    }
+   });
+
+
+
+
+
   return (
     <div className="container mx-auto">
+
+      <Toaster position='top-center' reverseOrder={false}>
+
+      </Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
@@ -17,13 +40,13 @@ export default function Username() {
             </span>
           </div>
 
-          <form className='py-1'>
+          <form className='py-1' onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-3">
               <img src={avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6 ">
-              <input className={styles.textbox} type="text" name="" placeholder='Username' id="" />
+              <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username' />
               <button className={styles.btn} type='submit'>Let's Go</button>
             </div>
 
